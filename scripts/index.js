089fd7beb7c77edb3b94de50ea73d15382f0e688
saivignesh97 <vsai97@gmail.com>
2020-02-23 00:21:04 +0530

    Added Sign in modal

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,13 @@ function hideSignUpModal() {
     var modal = document.getElementById("signup-modal");
     modal.style.display = "none";
 }
+
+function showSignInModal() {
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "flex";
+}
+
+function hideSignInModal() {
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "none";
+}
